a65008b9a230f7e16bd97359240f169c8a95e9fe
heejin <dongdong7251@naver.com>
2022-02-27 22:03:07 +0900

    add favorite page

---
+++ client/src/components/Config.js
@@ -0,0 +1,11 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'f878080181e5d968432febfeecfbcd17';
+export  const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+export const POSTER_SIZE = 'w500'
